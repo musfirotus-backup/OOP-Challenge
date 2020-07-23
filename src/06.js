6fbce7fe14c99b04d10e3b6529cabaa4984ffe7a
Musfirotus Sa'adah <musfirotus@gmail.com>
2020-07-23 08:20:25 +0700

    Hmm, file ketiga tapi soal nomor 6..  :(

---
+++ src/06.js
@@ -0,0 +1,105 @@
+let data = [{
+        id: 1,
+        name: "Fira",
+        username: "HomeVase",
+        pass: "111",
+        user: true,
+    },
+    {
+        id: 2,
+        name: "Rara",
+        username: "Cookies",
+        pass: "222",
+        user: false,
+    },
+];
+
+
+class Auth {
+    constructor(username, password, data) {
+        this.data = data;
+        this.username = username;
+        this.password = password;
+        this.status = false;
+        this.current = "Tidak Ada Data";
+        this.date = new Date().toLocaleString();
+    }
+
+    login() {
+        for (let a in data) {
+            if (this.username == data[a].username) {
+                console.log(`Selamat datang ${data[a].username}`);
+                this.status = true;
+                this.current = data[a];
+                return true;
+            }
+        }
+        console.log("gagal login");
+    }
+    validate(name) {
+        for (let a in data) {
+            if (this.username == data[a].username) {
+                console.log(`Tervalidasi! ${data[a].username} adalah user`);
+                this.current = data[a];
+                return true;
+            }
+        }
+        console.log(true);
+    }
+    logout() {
+        this.username = "";
+        this.password = "";
+        this.status = false;
+        this.current = "Tidak ada data";
+        console.log(`Anda Telah Logout`);
+        this.date = new Date().toLocaleString();
+
+    }
+    user() {
+        console.log(this.current);
+    }
+
+    id(id) {
+        data.forEach((data) => {
+            if (data.id == id) console.log(data);
+        });
+    }
+    check() {
+        let status =
+            this.status === true ? "Akun tidak pernah Login" : "Akun pernah Login";
+        console.log(status);
+        return true;
+    }
+    guest() {
+        console.log(this.status === false ? "Akun Sedang Logout" : "Akun Sedang Login");
+        return false;
+    }
+    lastLogin() {
+        console.log(`Login terakhir :`, this.status === true ? new Date().toLocaleString() : this.date);
+    }
+}
+let refactory = new Auth("HomeVase", "111", data);
+refactory.login();
+refactory.validate();
+refactory.logout();
+refactory.user();
+refactory.id();
+refactory.check();
+refactory.guest();
+refactory.lastLogin();
+// refactory.id(2);
+
+// Auth.login({
+//     username: 'root',
+//     password: 'secret'
+// })
+// Auth.validate({
+//     username: 'root',
+//     password: 'secret'
+// })
+// Auth.logout()
+// Auth.user()
+// Auth.id()
+// Auth.check()
+// Auth.guest()
+// Auth.lastLogin()
